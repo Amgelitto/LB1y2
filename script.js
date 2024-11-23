9e1788beeba284797d437e1901e6b219ed18d319
amgelitto <miguel.florez.ttc.311@unilibre.edu.co>
2024-11-22 21:58:52 -0500

    se corrige un error y se estilizan strings

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let num1 = 0;
 let num2 = 0;
 let operacion = '';
 
-// Aquí hacemos la función para realizar operaciones matemáticas
 function realizarOperacion(num1, num2, operacion) {
   if (operacion === 'suma') {
     return num1 + num2;
@@ -21,26 +20,31 @@ function realizarOperacion(num1, num2, operacion) {
   }
 }
 
-// Aquí validamos la operación ingresada
 function validarOperacion(operacion) {
   return ['suma', 'resta', 'multiplicacion', 'division'].includes(operacion);
 }
 
-// Bucle para realizar múltiples operaciones
 let continuar = true;
 
 while (continuar) {
-  num1 = parseFloat(prompt('Ingrese el primer número:'));
-  num2 = parseFloat(prompt('Ingrese el segundo número:'));
-  operacion = prompt('Ingrese la operación (suma, resta, multiplicacion, division) o "salir" para terminar:').toLowerCase();
+  num1 = parseFloat(prompt('Ingresa un numero:'));
+  num2 = parseFloat(prompt('Ingresa otro número:'));
+  operacion = prompt('Ingrese la operación: suma, resta, multiplicacion, division o salir para terminar:');
+  
+  // Validación para evitar error de toLowerCase en valor null
+  if (operacion) {
+    operacion = operacion.toLowerCase();
 
-  if (operacion === 'salir') {
-    continuar = false;
-    alert('¡Gracias por usar la calculadora! Adiós.');
-  } else if (validarOperacion(operacion)) {
-    const resultado = realizarOperacion(num1, num2, operacion);
-    alert(`El resultado de la operación ${operacion} es: ${resultado}`);
+    if (operacion === 'salir') {
+      continuar = false;
+      alert('¡shau!, adiós.');
+    } else if (validarOperacion(operacion)) {
+      const resultado = realizarOperacion(num1, num2, operacion);
+      alert(`El resultado de la  ${operacion} es: ${resultado}`);
+    } else {
+      alert('Algo hiciste mal, como para variar.');
+    }
   } else {
-    alert('Operación no válida. Por favor, intente de nuevo.');
+    alert('Escribe la operación la próxima vez por favor, ome.');
   }
 }
